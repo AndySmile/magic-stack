cc9316a2b14f33d21d429ce4c543d0b7744afb3e
Andy Liebke <coding@andysmiles4games.com>
2018-02-25 22:14:11 +0100

    Add empty GameController

---
+++ Game/MagicStack/MagicStackLibrary/Src/Controller/GameController.cpp
@@ -0,0 +1,24 @@
+#include "GameController.h"
+
+namespace MagicStack
+{
+    void CGameController::Init(CControllerTransition& transition)
+    {
+
+    }
+
+    void CGameController::OnEnter()
+    {
+
+    }
+
+    void CGameController::OnLeave()
+    {
+
+    }
+
+    std::string CGameController::GetAlias() const
+    {
+        return "game";
+    }
+}
---
+++ Game/MagicStack/MagicStackLibrary/Src/Controller/GameController.h
@@ -0,0 +1,23 @@
+#ifndef __MAGICSTACK_CONTROLLER_GAMECONTROLLER_H__
+#define __MAGICSTACK_CONTROLLER_GAMECONTROLLER_H__
+
+#include "BaseMenuController.h"
+
+namespace MagicStack
+{
+    struct IAction;
+
+    class CGameController : public CBaseMenuController
+    {
+        public:
+            CGameController() = default;
+            virtual ~CGameController() = default;
+
+            virtual void Init(CControllerTransition& transition) override;
+            virtual void OnEnter() override;
+            virtual void OnLeave() override;
+            virtual std::string GetAlias() const;
+    };
+}
+
+#endif
