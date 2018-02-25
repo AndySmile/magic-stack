72e2716b5abfa42fb4c711fb44bac45c695ebd03
Andy Liebke <coding@andysmiles4games.com>
2018-02-25 22:03:21 +0100

    Add HelpController to the FrontController

--- Game/MagicStack/MagicStackLibrary/Src/Controller/MainController.cpp
+++ Game/MagicStack/MagicStackLibrary/Src/Controller/MainController.cpp
@@ -1,11 +1,13 @@
 #include "MainController.h"
 #include "MenuController.h"
+#include "HelpController.h"
 
 namespace MagicStack
 {
     CMainController::CMainController()
     {
         this->FrontController.AddController(new CMenuController());
+        this->FrontController.AddController(new CHelpController());
     }
 
     void CMainController::Run()
