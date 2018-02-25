488c9a3d93405c77369ad653617b33b1027dcca6
Andy Liebke <coding@andysmiles4games.com>
2018-02-25 19:10:04 +0100

    Change header guard name for the macros

--- Game/MagicStack/MagicStackLibrary/Src/Helper/Macros.h
+++ Game/MagicStack/MagicStackLibrary/Src/Helper/Macros.h
@@ -1,5 +1,5 @@
-#ifndef __MENU_MACROS_h__
-#define __MENU_MACROS_h__
+#ifndef __MAGICSTACK_HELPER_MACROS_h__
+#define __MAGICSTACK_HELPER_MACROS_h__
 
 #define DELETE_OBJECT(object) if (object != nullptr) { delete object; object = nullptr; }
 
